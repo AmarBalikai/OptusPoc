a650109fe2b1033e7d50341af483323b89ce8b69
Amar Balikai <amar.balikai108@gmail.com>
2020-05-22 21:00:36 +0530

    Initial commit

---
+++ app/src/main/java/com/techm/optuspoc/UserInformation.kt
@@ -0,0 +1,28 @@
+package com.techm.optuspoc
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+
+class UserInformation : AppCompatActivity() {
+    var tag:String=UserInformationFragment::class.java.name
+    var name=UserInformationFragment()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_information)
+
+        val fragmentManager: FragmentManager = supportFragmentManager
+        // check if the fragment is in back stack
+        // check if the fragment is in back stack
+        val fragmentPopped: Boolean = fragmentManager.popBackStackImmediate(tag, 0)
+        if (fragmentPopped) {
+            // fragment is pop from backStack
+        } else {
+            val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.fragment_container, name, tag)
+            fragmentTransaction.addToBackStack(tag)
+            fragmentTransaction.commit()
+        }
+    }
+}
